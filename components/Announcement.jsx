6cbd732a39cb2a0710da9b6fcda04e2f468e4827
sohanr-cyber <sohanur25800@gmail.com>
2023-11-11 22:45:55 +0600

    under development

---
+++ components/Announcement.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+import styles from "@/styles/Announcement.module.css";
+const Announcement = () => {
+  return <div className={styles.wrapper}>This Site is Under development</div>;
+};
+
+export default Announcement;
--- components/Layout.jsx
+++ components/Layout.jsx
@@ -1,10 +1,13 @@
 import React from "react";
 import Footer from "./Footer";
 import Navbar from "./Navbar";
+import Announcement from "./Announcement";
 
 const Layout = ({ children }) => {
   return (
     <>
+      {" "}
+      <Announcement />
       <Navbar />
       {children}
       <Footer />
