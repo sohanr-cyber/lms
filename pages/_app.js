40464d3818a92abcd8f18a299f3e4d4978e1ca87
sohanr-cyber <sohanur25800@gmail.com>
2023-12-02 16:51:05 +0600

    seo

--- pages/_app.js
+++ pages/_app.js
@@ -4,11 +4,41 @@ import { GoogleOAuthProvider } from "@react-oauth/google";
 import { store } from "@/redux/store";
 import { Provider } from "react-redux";
 import NextNProgress from "nextjs-progressbar";
+import { DefaultSeo } from "next-seo";
+const site = "https://schoolpress.vercel.app/";
+const title = "Schoolpress - Your Learning Journey Begins Here!";
+const description =
+  "choolPress, the leading online Learning Management System (LMS) empowering educators and learners. Explore interactive courses, analytics, and collaborative tools for a holistic learning experience.";
 
 export default function App({ Component, pageProps }) {
   return (
     <>
-      {" "}
+      <DefaultSeo
+        title={title}
+        description={description}
+        openGraph={{
+          type: "website",
+          url: site,
+          title: title,
+          description: description,
+          images: [
+            {
+              url: "/images/schoolpress.png",
+              width: 1200,
+              height: 630,
+              alt: "schoolpress",
+            },
+          ],
+        }}
+        twitter={{
+          handle: "@schoolpress",
+          site: "@schoolpress",
+          cardType: "summary_large_image",
+          title: title,
+          description: description,
+          image: "/images/schoolpress.png",
+        }}
+      />
       <NextNProgress height={3} />
       <Provider store={store}>
         <GoogleOAuthProvider clientId="182276852693-qkrjpntbo95umfsgphvclpgr8kp56kci.apps.googleusercontent.com">
--- pages/division/[slug]/[program]/[course]/index.js
+++ pages/division/[slug]/[program]/[course]/index.js
@@ -2,11 +2,40 @@ import CourseContent from "@/components/CourseContent";
 import Heading from "@/components/Heading";
 import url from "@/configure";
 import axios from "axios";
+import { NextSeo } from "next-seo";
 import React from "react";
-
+import { useRouter } from "next/router";
 const Course = ({ data, content }) => {
+  const router = useRouter();
   return (
     <>
+      {" "}
+      <NextSeo
+        title={data.title}
+        description={data.description}
+        openGraph={{
+          type: "website",
+          url: `${url}/${router.query.slug}/${router.query.program}/${router.query.course}`,
+          title: data.title,
+          description: data.description,
+          images: [
+            {
+              url: data.image,
+              width: 1200,
+              height: 630,
+              alt: data.title,
+            },
+          ],
+        }}
+        twitter={{
+          handle: "@schoolpress",
+          site: "@schoolpress",
+          cardType: "summary_large_image",
+          title: data.title,
+          description: data.description,
+          image: data.image,
+        }}
+      />
       <Heading data={data} />
       <CourseContent contents={content} course={data} />
     </>
